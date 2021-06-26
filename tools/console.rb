3fe9e8e01835d6a6aae21c133cd1363214c97c9e
Pranik Chainani <pranikc@gmail.com>
2021-06-26 00:26:12 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -10,4 +10,12 @@ end
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs) 
